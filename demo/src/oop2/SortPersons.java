511e1c2df676add7b5f3e191eaf8edd2a5b9427a
srikanthpragada <srikanthpragada@gmail.com>
2023-11-01 18:56:21 +0530

    01-NOV

--- demo/src/oop2/SortPersons.java
+++ demo/src/oop2/SortPersons.java
@@ -1,6 +1,14 @@
 package oop2;
 
 import java.util.Arrays;
+import java.util.Comparator;
+
+class NameCompare implements Comparator<Person> {
+	@Override
+	public int compare(Person p1, Person p2) {
+	    return p1.getName().compareTo(p2.getName()); 
+	}
+}
 
 class Person implements Comparable<Person> {
 	private String name;
@@ -23,13 +31,31 @@ class Person implements Comparable<Person> {
 		System.out.println("compareTo");
 		return this.age - other.age;
 	}
+	
+	public String getName() {
+		return this.name;
+	}
 }
 
 public class SortPersons {
 	public static void main(String[] args) {
-		Person[] persons = { new Person("A", 30), new Person("C", 20), new Person("B", 40) };
+		Person[] persons = { new Person("AA", 30), new Person("C", 20), new Person("BBB", 40) };
 
-		Arrays.sort(persons);
+		//Arrays.sort(persons);
+		//Arrays.sort(persons,  new NameCompare());
+		
+//		Arrays.sort(persons, 
+//				(p1, p2) -> p1.getName().compareTo(p2.getName()));
+		
+		Arrays.sort(persons, 
+				 new Comparator<Person>() {
+					@Override
+					public int compare(Person p1, Person p2) {
+						 return p1.getName().length() - 
+								   p2.getName().length();
+					}
+			
+		});
 
 		for (var p : persons)
 			System.out.println(p);
